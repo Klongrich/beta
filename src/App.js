dfa92f2718a1482fc9d8911d26fd8bb09dd08133
klongrich <longrichk@gmail.com>
2021-09-18 12:00:08 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react";
+
+function App() {
+
+  useEffect(() => {
+    console.log("hello world");
+  })
+
+  return (
+    <>
+      <h2> Hello World </h2>
+    </>
+  );
+}
+
+export default App;
