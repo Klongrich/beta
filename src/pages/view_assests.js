af17facece0e7a81f53b0ba7a7d4a6221510ef34
klongrich <longrichk@gmail.com>
2021-09-21 00:57:57 -0400

    adding listing box

--- src/pages/view_assests.js
+++ src/pages/view_assests.js
@@ -6,6 +6,12 @@ import Button from "@material-ui/core/Button";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { createTheme } from '@material-ui/core/styles';
 
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogContent from "@material-ui/core/DialogContent";
+import { TextField } from "@material-ui/core"
+
+
 export const ImageBox = styled.div`
     height: 350px;
     width: 300px;
@@ -104,6 +110,14 @@ export const Filler = [
 export default function View_Assests({ address }) {
 
     const [userNFTs, setUserNFTs] = useState(Filler);
+    const [isListing, setItsListing] = useState(false);
+
+    const [listingName, setListingName] = useState(false);
+    const [listingImage, setListingImage] = useState(false);
+    const [listingApproved, setListingApproved] = useState(false);
+    const [listingPrice, setListingPrice] = useState(0);
+
+    const [confrimPrice, setConfrimPrice] = useState(false);
 
     function getCurrentAssests() {
         console.log("Before Fetch Address: " + address);
@@ -130,98 +144,163 @@ export default function View_Assests({ address }) {
             })
     }
 
+    //Implement Web3-js calls
+    function checkUsersWalletForApproval() {
+        setListingApproved(true);
+    }
+
+    function listCurrentAssest(image, name) {
+        checkUsersWalletForApproval();
+
+        setListingName(name);
+        setListingImage(image);
+        setItsListing(true);
+    }
+
+
+    function submitListing() {
+        if (listingApproved) {
+            //Use Material-UI
+            alert("listing is not approved!")
+            return (0);
+        } else if (!confrimPrice) {
+            setConfrimPrice(true)
+            return (0);
+        } else if (confrimPrice && !listingApproved) {
+            //function to submit listing with Web3-js call
+            console.log("Listing nft .....");
+        }
+    }
+
+    function handleToClose() {
+        setItsListing(false);
+        setConfrimPrice(false);
+    }
+
     useEffect(() => {
         getCurrentAssests();
     }, [])
 
-
     return (
         <>
-            <h1 Style="text-align: center;"> View User Assests </h1>
-
-            {/* <HeaderBox>
-            <h2> View Assests</h2>
-            <p Style="margin-left: 140px; margin-top: -40px;">- Created On - 12/12/2012</p>
-            <ul>
-                <li> Total NFT Value: $142,000 </li>
-                <li> Address Balance: 42.22 ETH </li>
-            </ul>
-
-            <br />
-            <p>Sort By -> Collection, price, purchase date, etc ...</p>
-            </HeaderBox> */}
-
-            <AssesetFilter>
-                <h2 Style="text-align: center; font-size: 30px"> Assest Filter </h2>
-
-                <div>
-                    <ThemeProvider theme={theme}>
-
+            <ThemeProvider theme={theme}>
+                <h1 Style="text-align: center;"> View User Assests </h1>
+                <AssesetFilter>
+                    <h2 Style="text-align: center; font-size: 30px"> Assest Filter </h2>
+                    <div>
                         <Button style={{ minWidth: '142px' }} size="large" variant="contained" color="primary">  Recent  </Button>
                         <br /> <br />
                         <Button style={{ minWidth: '142px' }} size="large" variant="contained" color="primary">  On Sale </Button>
                         <br /> <br />
-
-
                         <div Style="text-align: right; margin-top: -120px; margin-right: 18px;">
                             <Button style={{ minWidth: '142px' }} size="large" variant="contained" color="primary">  On Auction </Button>
                             <br /> <br />
                             <Button style={{ minWidth: '142px' }} size="large" variant="contained" color="primary">  Has Offers </Button>
                         </div>
-                    </ThemeProvider>
-                </div>
-
-                <br />
-                <div Style="border-top: 1px solid black; 
+                    </div>
+                    <br />
+                    <div Style="border-top: 1px solid black; 
                             margin-right: 18px;
                             padding-bottom: 20px;
                             margin-top: 15px;">
-                    <h2> Collection </h2>
-                    <input />
-                    <br /> <br />
-                    <button>
-                        Search
+                        <h2> Collection </h2>
+                        <input />
+                        <br /> <br />
+                        <button>
+                            Search
                     </button>
-                </div>
-                <br />
-                <div Style="border-top: 1px solid black;
+                    </div>
+                    <br />
+                    <div Style="border-top: 1px solid black;
                             border-bottom: 1px solid black;
                             padding-bottom: 30px; 
                             margin-right: 18px;">
-                    <h2>Price: $USD</h2>
+                        <h2>Price: $USD</h2>
 
-                    <p>Min: </p>
-                    <input />
+                        <p>Min: </p>
+                        <input />
 
-                    <p>Max: </p>
-                    <input />
-                    <br /> <br />
-                    <button>
-                        Search
-                    </button>
-                </div>
-            </AssesetFilter>
-
-            {userNFTs.map((data) =>
-                <>
-                    <ImageBox>
-
-                        <h3 Style="margin-top: -7px;"> Name: {data.name} </h3>
-
-                        <p>Price Bought: 1.42 ETH </p>
-                        <p>Profit / Losst: <strong Style="color: green"> +$42,000 </strong> </p>
-
-                        <img src={data.image_url} height={210} width={210} alt="" />
-                        {/* {<p>Owner: {data.owner.user.username}</p>} */}
-                        {/* <p>TokenID: {data.description} </p> */}
-                        {/* <p>URL Link: {data.image_url} </p> */}
+                        <p>Max: </p>
+                        <input />
                         <br /> <br />
                         <button>
-                            Sell NFT
+                            Search
+                    </button>
+                    </div>
+                </AssesetFilter>
+
+                {userNFTs.map((data) =>
+                    <>
+                        <ImageBox>
+
+                            <h3 Style="margin-top: -7px;"> Name: {data.name} </h3>
+
+                            <p>Price Bought: 1.42 ETH </p>
+                            <p>Profit / Losst: <strong Style="color: green"> +$42,000 </strong> </p>
+
+                            <img src={data.image_url} height={210} width={210} alt="" />
+                            {/* {<p>Owner: {data.owner.user.username}</p>} */}
+                            {/* <p>TokenID: {data.description} </p> */}
+                            {/* <p>URL Link: {data.image_url} </p> */}
+                            <br /> <br />
+                            <button onClick={() => listCurrentAssest(data.image_url, data.name)}>
+                                Sell NFT
                         </button>
-                    </ImageBox>
-                </>
-            )}
+                        </ImageBox>
+                    </>
+                )}
+
+                <Dialog open={isListing} onClose={handleToClose}>
+                    <DialogContent>
+                        <div Style="width: 300px; text-align: center;">
+                            <DialogContentText>
+                                <h2 Style="text-align: center;">List Your Item</h2>
+
+                                <h3 Style="text-align: center;">{listingName}</h3>
+                                <img Style="text-align: center;" src={listingImage} height={240} width={240} alt="" />
+
+                                <br /> <br />
+                                <TextField style={{ minWidth: "240px " }}
+                                    id="outlined-basic"
+                                    label="Price"
+                                    variant="outlined"
+                                    onChange={e => setListingPrice(e.target.value)} />
+                                <br /> <br />
+
+                                {listingApproved &&
+                                    <>
+                                        {/* Create Function to send meta to smartcontract */}
+                                        <Button onClick={() => setListingApproved(false)}
+                                            style={{ minWidth: "240px" }}
+                                            color="primary"
+                                            variant="outlined">
+                                            Approve Wallet
+                                    </Button>
+                                        <br /> <br />
+                                    </>
+                                }
+
+                                {confrimPrice &&
+                                    <>
+                                        <Button>
+                                            Confrim Price: {listingPrice} ETH?
+                                        </Button>
+                                        <br /> <br />
+                                    </>
+                                }
+
+                                <Button onClick={() => submitListing()}
+                                    style={{ minWidth: "240px" }}
+                                    color="secondary"
+                                    variant="outlined">
+                                    {!confrimPrice && <> List </>}
+                                    {confrimPrice && <> Confrim </>}
+                                </Button>
+                            </DialogContentText>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </ThemeProvider>
         </>
     )
 }
